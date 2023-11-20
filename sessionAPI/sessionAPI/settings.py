a93be833a2b18fce142945a3f26e60647d67e2af
Aniketh Babu <aniketh@aniketh.lan>
2023-11-20 10:58:30 -0500

    Followed middleware lesson and created custom middleware

--- sessionAPI/sessionAPI/settings.py
+++ sessionAPI/sessionAPI/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'sessionApp.middleware.MiddleWareLifeCycle',
+    'sessionApp.middleware.ExceptionHandlingMiddleware',
 ]
 
 ROOT_URLCONF = 'sessionAPI.urls'
