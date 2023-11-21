13597e5c10210d27bcbd5d454c80c7dc67c5e623
Aniketh Babu <aniketh@aniketh.lan>
2023-11-21 11:56:54 -0500

    went through orm relationships section in udemy

--- modelDemo/empApp/models.py
+++ modelDemo/empApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import IntegerField
 
 # Create your models here.
 class Employee(models.Model):
@@ -6,3 +7,30 @@ class Employee(models.Model):
     lastName=models.CharField(max_length=30)
     salary=models.FloatField() 
     email=models.CharField(max_length=35)
+
+class Programmer(models.Model):
+    name = models.CharField(max_length=20)
+    sal = models.IntegerField()
+
+class Project(models.Model):
+    name = models.CharField(max_length=30)
+    programmers = models.ManyToManyField(Programmer)
+
+class Customer(models.Model):
+    name = models.CharField(max_length=30)
+
+class PhoneNumber(models.Model):
+    type = models.CharField(max_length=10)
+    number = models.CharField(max_length=10)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+class Person(models.Model):
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+    age = models.IntegerField()
+
+class License(models.Model):
+    type = models.CharField(max_length=10)
+    ValidFrom = models.DateField()
+    ValidTo = models.DateField()
+    person = models.OneToOneField(Person, on_delete=models.CASCADE)
