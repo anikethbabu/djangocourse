ecfc7a8bc8ba0ce619597df11595b4c41b4e9fe2
Aniketh Babu <aniketh@aniketh.lan>
2023-11-21 16:53:34 -0500

    added delete patient functionality

--- clinicals/clinicals/urls.py
+++ clinicals/clinicals/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.PatientListView.as_view(), name='index'),
     path('create/', views.PatientCreateView.as_view()),
-    path('update/<int:pk>', views.PatientUpdateView.as_view()),
+    path('update/<int:pk>/', views.PatientUpdateView.as_view()),
+    path('delete/<int:pk>/', views.PatientDeleteView.as_view()),
 ]
