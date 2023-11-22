8572c0a537076393af90348b8ea7addd9c76531d
Aniketh Babu <aniketh@aniketh.lan>
2023-11-22 16:10:10 -0500

    added analyze view and patient bmi calculation based on height and weight

--- clinicals/clinicals/urls.py
+++ clinicals/clinicals/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('update/<int:pk>/', views.PatientUpdateView.as_view()),
     path('delete/<int:pk>/', views.PatientDeleteView.as_view()),
     path('addData/<int:pk>/', views.addData),
+    path('analyze/<int:pk>/', views.analyze),
 ]
--- clinicals/clinicalsApp/views.py
+++ clinicals/clinicalsApp/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from clinicalsApp.forms import ClinicalDataForm
-from clinicalsApp.models import Patient
+from clinicalsApp.models import ClinicalData, Patient
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from django.shortcuts import redirect
 
 # Create your views here.
 class PatientListView(ListView):
@@ -34,3 +33,19 @@ def addData(request, **kwargs):
             form.save()
         return redirect('/')
     return render(request,'clinicalsApp/clinicaldata_form.html',{'form':form, 'patient': patient})
+
+def analyze(request, **kwargs):
+    data = ClinicalData.objects.filter(patient_id=kwargs['pk'])
+    responseData = []
+    for eachEntry in data:
+        if eachEntry.componentName == 'hw':
+            heightAndWeight = eachEntry.componentValue.split('/')
+            if len(heightAndWeight) > 1:
+                feetToMetres = float(heightAndWeight[0]) * 0.4536
+                BMI = (float(heightAndWeight[1])) / (feetToMetres*feetToMetres)
+                bmiEntry = ClinicalData()
+                bmiEntry.componentName = 'BMI'
+                bmiEntry.componentValue = str(BMI) 
+                responseData.append(bmiEntry)
+        responseData.append(eachEntry)
+    return render(request, 'clinicalsApp/generateReport.html', {'data': responseData})
